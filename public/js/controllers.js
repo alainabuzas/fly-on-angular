217c98ed5fe974f5cf752437192dc5c02bcf336a
alainabuzas <alaina.buzas@gmail.com>
2017-05-15 08:35:21 -0700

    errors gone

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,6 +1,6 @@
-var controller = angular.module('AirplanesControllers', ['AirplaneServices']);
+var controllers = angular.module('AirplanesControllers', ['AirplaneServices']);
 
-controller.controllers('AirplanesControllers', ['$scope', 'Airplane', function($scope, Airplane) {
+controllers.controller('AirplanesControllers', ['$scope', 'Airplane', function($scope, Airplane) {
     Airplane.showAllPlanes().then(function(res) {
         $scope.airplanes = res.data;
     }).catch(function(err) {
@@ -8,7 +8,7 @@ controller.controllers('AirplanesControllers', ['$scope', 'Airplane', function($
     });
 }]);
 
-controller.controllers('AirplaneInfoCtrl', ['$scope', '$stateParams', 'Airplane', function($scope, $stateParams, Airplane) {
+controllers.controller('AirplaneInfoCtrl', ['$scope', '$stateParams', 'Airplane', function($scope, $stateParams, Airplane) {
     $scope.airplane = {};
     Airplane.showPlane($stateParams.id).then(function(res) {
         $scope.airplane = res.data;
